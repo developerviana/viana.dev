ea1e8a3943210a359787d14967622e0ae48037f0
Victor Lucas Costa Viana <victor.viana@katrium.local>
2025-05-02 17:42:59 -0300

    feat: card de apresentação ok

--- vianadevfront/src/app/components/header/header.component.ts
+++ vianadevfront/src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
- import { NgOptimizedImage } from '@angular/common';
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { MenuComponent } from "../menu/menu.component";
+import { CommonModule } from '@angular/common';
 
 
 @Component({
@@ -8,7 +9,8 @@ import { MenuComponent } from "../menu/menu.component";
   standalone: true,
   imports: [
     NgOptimizedImage,
-    MenuComponent
+    MenuComponent,
+    CommonModule
 ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
--- vianadevfront/src/app/components/home/home.component.ts
+++ vianadevfront/src/app/components/home/home.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from '../header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,
+            CommonModule
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
 
+  email: string = 'vianacommerce@gmail.com';
+
+  showAlert: boolean = false;
+
+  copyEmailToClipboard() {
+    navigator.clipboard.writeText(this.email).then(() => {
+      this.showAlert = true;
+      setTimeout(() => {
+        this.showAlert = false;
+      }, 1000); 
+    }).catch(err => {
+      console.error('Erro ao copiar:', err);
+    });
+  }
 }
