27b11026dbafbda8e2d54131f57fee3e33d32e5f
vianadevelop <flood.com@hotmail.com>
2025-05-14 15:18:00 -0300

    fix: scrool menu and create skills

--- vianadevfront/src/app/components/menu/menu.component.ts
+++ vianadevfront/src/app/components/menu/menu.component.ts
@@ -3,10 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
 export class MenuComponent {
+  scrollToSection(event: Event, id: string) {
+    event.preventDefault(); 
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+}
 
 }
--- vianadevfront/tailwind.config.js
+++ vianadevfront/tailwind.config.js
@@ -1,10 +1,14 @@
 
+	/** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
 	  "./src/**/*.{html,ts}", 
 	],
 	theme: {
 	  extend: {
+		colors: {
+			'cinza-escuro': '#262626',
+		},
 		fontFamily: {
 		  montserratAlt: ['"Montserrat Alternates"', 'sans-serif'],
 		},
